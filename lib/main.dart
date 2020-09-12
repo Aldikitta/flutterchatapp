60d854b2c9ef790886b513c2e41963a35141bb66
Aldikitta <andi.kitta77@gmail.com>
2020-09-12 11:36:54 +0800

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import './screens/chatScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
